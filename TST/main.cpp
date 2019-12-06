a759f31894943b9c3b7b7bac9a2bc8b9c5b214a0
jakobteuffel <j.teuffel@web.de>
2019-12-06 16:31:13 +0100

    Make Diagonalize Basic Matrix

--- TST/main.cpp
+++ TST/main.cpp
@@ -6,16 +6,29 @@
 //  Copyright © 2019 Jakob Teuffel. All rights reserved.
 //
 #define EIGEN_USE_MKL
+#define MATRIX_SIZE 1000
 
 #include <iostream>
 #include <Eigen/Dense>
-using Eigen::MatrixXd;
+#include <omp.h>
+
 int main()
 {
-    MatrixXd m(2,2);
-    m(0,0) = 0;
-    m(1,0) = 1;
-    m(0,1) = -1;
-    m(1,1) = 0;
-    std::cout << m << std::endl;
+    omp_set_num_threads(4);
+    Eigen::MatrixX<std::complex<double>>m(MATRIX_SIZE,MATRIX_SIZE);
+    #pragma omp parallel for
+    for (size_t i = 0; i < MATRIX_SIZE; i++) {
+        #pragma omp parallel for
+        for (size_t j = 0; j < MATRIX_SIZE; j++) {
+            double z = double(j)-double(i);
+            double x = double(j)+double(i);
+            m(i,j) = std::complex<double>(1+(x-MATRIX_SIZE/2-2)/(z*z+1),0)*exp(std::complex<double>(0,z));
+        }
+    }
+    //std::cout << m << std::endl;
+    Eigen::ComplexEigenSolver<Eigen::MatrixX<std::complex<double>>> Solver(MATRIX_SIZE);
+    Solver.compute(m);
+    Eigen::VectorX<double> EigenValues = Solver.eigenvalues().col(0).real();
+    std::sort(EigenValues.data(),EigenValues.data()+EigenValues.size());
+    std::cout << "eigenvalues:" << std::endl << EigenValues << std::endl;
 }
