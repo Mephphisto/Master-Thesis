f23b41fd78a2e222b938e212d0f649994a055910
Jakob Teuffel <j.teuffel@icloud.com>
2020-02-04 20:16:06 +0100

    - Stupid Cmake Fix

---
+++ TST/build_teerm/CMakeFiles/TST.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TST.dir/main.cpp.o"
+  "TST"
+  "TST.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
