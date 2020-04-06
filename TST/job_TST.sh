65bec9982bc5c8c6f97ac6bc8a36709e23b2564c
jakobteuffel <j.teuffel@web.de>
2020-04-06 18:29:43 +0200

    - Fixed jobfile

--- TST/job_TST.sh
+++ TST/job_TST.sh
@@ -6,10 +6,11 @@
 git commit -am " Prepare Run $date"
 git pull
 module load intel
-mkdir "build"
-cd build
+mkdir "run"
+cd run
 cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=16 -DGRID=100 -DT_RES=800 -DUSE_MAGMA=FALSE
 make -j 16
 ./TST
+git add .
 git commit -am "Run finished $date"
 git push
