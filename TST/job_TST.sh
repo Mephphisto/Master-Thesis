2890ead1f044ac11dd4300db34d8138129af0750
jakobteuffel <j.teuffel@web.de>
2020-04-06 18:27:04 +0200

    - Fixed jobfile

--- TST/job_TST.sh
+++ TST/job_TST.sh
@@ -1,13 +1,14 @@
 #$-q idefix.q,obelix.q
 #$ -l h_vmem=4G
 #$ -l h_cpu=48:00:00
-#$ -pe smp 4
+#$ -pe smp 16
+#$-M j.teuffel@icloud.com -m ase
 git commit -am " Prepare Run $date"
 git pull
 module load intel
 mkdir "build"
 cd build
-cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=4 -DGRID=60 -DT_RES=800 -DUSE_MAGMA=FALSE
+cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=16 -DGRID=100 -DT_RES=800 -DUSE_MAGMA=FALSE
 make -j 16
 ./TST
 git commit -am "Run finished $date"
