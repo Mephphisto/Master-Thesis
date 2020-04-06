ed44ce84d30f32819f0d2b081fa37331770aa85e
jakobteuffel <j.teuffel@web.de>
2020-04-06 15:44:29 +0200

    - Made Jobfile

---
+++ TST/job_TST.sh
@@ -0,0 +1,13 @@
+#$ -q idefix.q
+#$ -M j.teuffel@icloud.com and -m b e a
+#$-l hvmem=4G
+#$-l hcpu=48:00:00
+#$-pe smp 4
+git pull
+module load intel
+cd build
+cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=4 -DGRID=10 -DT_RES=800 -DUSE_MAGMA=FALSE
+make -j 16
+./TST
+git commit -m "Run finished $date"
+git push
