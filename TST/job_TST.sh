47e0c5cbe1ec31520b32b2b3b82443729beec2f4
jakobteuffel <j.teuffel@web.de>
2020-04-06 15:55:43 +0200

    - Made Jobfile

--- TST/job_TST.sh
+++ TST/job_TST.sh
@@ -3,6 +3,7 @@
 #$ -l h_vmem=4G
 #$ -l h_cpu=48:00:00
 #$ -pe smp 4
+git commit -am " Prepare Run $date"
 git pull
 module load intel
 mkdir "build"
@@ -10,5 +11,5 @@ cd build
 cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=4 -DGRID=10 -DT_RES=800 -DUSE_MAGMA=FALSE
 make -j 16
 ./TST
-git commit -m "Run finished $date"
+git commit -am "Run finished $date"
 git push
