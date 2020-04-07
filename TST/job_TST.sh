671a0e2049cefbe89d4b412e78c70d6ea75cf07e
jakobteuffel <j.teuffel@web.de>
2020-04-07 10:39:53 +0200

    - Cleanup - Fixed Job file

--- TST/job_TST.sh
+++ TST/job_TST.sh
@@ -1,7 +1,7 @@
 #$-q idefix.q,obelix.q
 #$ -l h_vmem=2G
 #$ -l h_cpu=48:00:00
-#$ -pe smp 32
+#$ -pe smp 8
 #$-M j.teuffel@icloud.com -m ase
 #$ -cwd
 git commit -am " Prepare Run $date"
@@ -9,7 +9,7 @@ git pull
 module load intel
 mkdir "run"
 cd run
-cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=32 -DGRID=100 -DT_RES=800 -DUSE_MAGMA=FALSE
+cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=8 -DGRID=85 -DT_RES=800 -DUSE_MAGMA=FALSE
 make -j 16
 ./TST
 git add .
