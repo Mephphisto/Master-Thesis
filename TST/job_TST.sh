7977b39cd832572a9fce0ad98799fc5742ee7e9f
Jakob Teuffel <jteuffel@idefix910.physnet.uni-hamburg.de>
2020-04-07 16:59:54 +0200

    fixed module load library required intel 2020

--- TST/job_TST.sh
+++ TST/job_TST.sh
@@ -1,5 +1,5 @@
 #$-q idefix.q,obelix.q
-#$ -l h_vmem=1G
+#$ -l h_vmem=2G
 #$ -l h_cpu=168:00:00
 #$ -pe smp 32
 #$-M j.teuffel@icloud.com -m ase
@@ -8,7 +8,7 @@ git pull
 module load intel/2020
 mkdir "run $date"
 cd run
-cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=1 -DGRID=10 -DT_RES=1200 -DUSE_MAGMA=FALSE -DVERBOSE=""
+cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=32 -DGRID=85 -DT_RES=1200 -DUSE_MAGMA=FALSE -DVERBOSE=""
 make -j 16
 echo "Running Program"
 ./TST
