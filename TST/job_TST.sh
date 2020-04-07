b5db0bdf3278b2f1ef4ce06ccd4af285b4709e57
Jakob Teuffel <jteuffel@obelix16.physnet.uni-hamburg.de>
2020-04-07 23:39:39 +0200

    Prepare Run

--- TST/job_TST.sh
+++ TST/job_TST.sh
@@ -1,7 +1,7 @@
-#$-q obelix.q
-#$ -l h_vmem=6G
+#$-q idefix.q,obelix.q
+#$ -l h_vmem=2G
 #$ -l h_cpu=168:00:00
-#$ -pe smp 96
+#$ -pe smp 32
 #$-M j.teuffel@icloud.com -m ase
 #$ -cwd
 git commit -am " Prepare Run $date"
@@ -9,8 +9,8 @@ git pull
 module load intel/2020
 mkdir "run $date"
 cd run
-cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=96 -DGRID=100 -DT_RES=800 -DUSE_MAGMA=FALSE -DVERBOSE=""
-make -j 8
+cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=32 -DGRID=85 -DT_RES=12000 -DUSE_MAGMA=FALSE -DVERBOSE=""
+make -j 16
 echo "Running Program"
 ./TST
 git add .
