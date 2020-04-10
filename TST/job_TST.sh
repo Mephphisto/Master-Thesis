ac1fff626a91d73456be464ca0b0b4a890998328
Jakob Teuffel <jteuffel@compile1.physnet.uni-hamburg.de>
2020-04-10 15:51:20 +0200

    Prepare Run

--- TST/job_TST.sh
+++ TST/job_TST.sh
@@ -10,7 +10,7 @@ git pull
 module load intel/2020
 mkdir "run Time Eval"
 cd "run Time Eval"
-cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=48 -DGRID=85 -DT_RES=800 -DUSE_MAGMA=FALSE -DVERBOSE="" -DUSE_GPU=FALSE -DTIME_EVOLUTION=TRUE
+cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=48 -DGRID=85 -DT_RES=800 -DUSE_MAGMA=FALSE -DVERBOSE="" -DUSE_GPU=FALSE -DTIME_EVOLUTION=TRUE -DCMAKE_PREFIX_PATH="~/boost_1_72_0/boost"
 make -j 4
 echo "Running Program"
 ./TST
