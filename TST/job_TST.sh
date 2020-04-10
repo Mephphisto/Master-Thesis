f3429a7dedd19989a400e499842ae1ec7148c3be
Jakob Teuffel <jteuffel@compile1.physnet.uni-hamburg.de>
2020-04-10 15:01:47 +0200

    Prepare Run

--- TST/job_TST.sh
+++ TST/job_TST.sh
@@ -7,8 +7,8 @@
 git commit -am " Prepare Run $date"
 git pull
 module load intel/2020
-mkdir "run $date"
-cd run
+mkdir "run Time Eval"
+cd "run Time Eval"
 cmake cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=48 -DGRID=85 -DT_RES=800 -DUSE_MAGMA=FALSE -DVERBOSE="" -DUSE_GPU=FALSE -DTIME_EVOLUTION=TRUE
 make -j 4
 echo "Running Program"
