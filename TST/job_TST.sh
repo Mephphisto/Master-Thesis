93ab8a52f0adb7ac3ced5880f2afaaaf533623e5
Jakob Teuffel <jteuffel@compile1.physnet.uni-hamburg.de>
2020-05-18 16:50:12 +0200

    Cluster run 18. Mai

--- TST/job_TST.sh
+++ TST/job_TST.sh
@@ -1,7 +1,7 @@
 #$-q idefix.q,obelix.q
 #$ -l h_vmem=2G
 #$ -l h_cpu=672:00:00
-#$ -pe smp 1
+#$ -pe smp 48
 #$-M j.teuffel@icloud.com -m ase
 #$ -cwd
 export BOOST_ROOT="~/boost_1_72_0/"
@@ -10,7 +10,7 @@ git pull
 module load intel/2020
 mkdir "run Time Eval"
 cd "run Time Eval"
-cmake cmake ..  -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=1 -DGRID=35 -DT_RES=100 -DUSE_MAGMA=FALSE -DVERBOSE=TRUE  -DUSE_GPU=FALSE -DTIME_EVOLUTION=TRUE -DCMAKE_PREFIX_PATH="~/boost_1_72_0/"
+cmake cmake ..  -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=48 -DGRID=4 -DT_RES=4 -DUSE_MAGMA=FALSE -DVERBOSE=FALSE  -DUSE_GPU=FALSE -DTIME_EVOLUTION=TRUE -DCMAKE_PREFIX_PATH="~/boost_1_72_0/"
 make -j
 echo "Running Program"
 ./TST
