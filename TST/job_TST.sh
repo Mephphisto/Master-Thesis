b47c79b49df9d4bed5d24af71569a2d175b5ad95
Jakob Teuffel <jteuffel@idefix078.physnet.uni-hamburg.de>
2020-06-03 04:22:09 +0200

    Prepare Run Release

--- TST/job_TST.sh
+++ TST/job_TST.sh
@@ -1,16 +1,16 @@
 #$-q idefix.q,obelix.q
 #$ -l h_vmem=2G
 #$ -l h_cpu=672:00:00
-#$ -pe smp 48
+#$ -pe smp 35
 #$-M j.teuffel@icloud.com -m ase
 #$ -cwd
 export BOOST_ROOT="~/boost_1_72_0/"
-git commit -am " Prepare Run small"
+git commit -am " Prepare Run Release"
 git pull
 module load intel/2020
-mkdir "run Time Eval vsmall"
-cd "run Time Eval vsmall "
-cmake cmake .. -DCMAKE_BUILD_TYPE=Release  -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=48 -DGRID=35 -DT_RES=1000 -DUSE_MAGMA=FALSE -DVERBOSE=FALSE  -DUSE_GPU=FALSE -DTIME_EVOLUTION=TRUE -DCMAKE_PREFIX_PATH="~/boost_1_72_0/"
+mkdir "run Time Eval Release f"
+cd "run Time Eval Release f"
+cmake cmake .. -DCMAKE_BUILD_TYPE=Release  -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=35 -DGRID=85 -DT_RES=200 -DUSE_MAGMA=FALSE -DVERBOSE=FALSE  -DUSE_GPU=FALSE -DTIME_EVOLUTION=TRUE -DCMAKE_PREFIX_PATH="~/boost_1_72_0/"
 make -j 8
 echo "Running Program"
 ./TST
