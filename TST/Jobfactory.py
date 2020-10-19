1cddfd95bc3b556f6ac8f7b869cbefce96d7200f
jakobteuffel <j.teuffel@web.de>
2020-10-19 16:54:02 +0200

    - Hotfix - M.determinant() throws error for unknown reason?

--- TST/Jobfactory.py
+++ TST/Jobfactory.py
@@ -4,7 +4,8 @@ header = """#$-q idefix.q,obelix.q
 #$ -l h_vmem=3G
 #$ -l h_cpu=999:00:00
 #$ -pe smp {0}
-#$-M j.teuffel@icloud.com -m ase
+#$ -M j.teuffel@icloud.com -m ase
+#$ -l h=!obelix11,!obelix12
 #$ -cwd
 
 path="run_Time_Eval_Benchmark_G{1}_Res{1}_Threads{0}"
@@ -13,7 +14,7 @@ echo $path
 module load intel/2020
 mkdir $path
 cd $path
-cmake cmake .. -DCMAKE_BUILD_TYPE=Release  -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS={0} -DMKL_PARALEL=TRUE -DTARGET_ARCHITECTURE=skylake -DGRID={1} -DT_RES={2} -DT_ROT=1.0 -DT_C=-2.0 -DW_START=-3 -DW_END=-3 -DW_RES=1 -DUSE_MAGMA=FALSE -DVERBOSE=FALSE  -DUSE_GPU=FALSE -DTIME_EVOLUTION=TRUE -DCMAKE_PREFIX_PATH="~/boost_1_72_0/"
+cmake cmake .. -DCMAKE_BUILD_TYPE=Release  -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS={0} -DMKL_PARALEL=TRUE -DTARGET_ARCHITECTURE=westmere -DGRID={1} -DT_RES={2} -DT_ROT=1.0 -DT_C=-2.0 -DW_START=-3 -DW_END=-3 -DW_RES=1 -DUSE_MAGMA=FALSE -DVERBOSE=FALSE  -DUSE_GPU=FALSE -DTIME_EVOLUTION=TRUE -DCMAKE_PREFIX_PATH="~/boost_1_72_0/"
 make -j 8
 echo "running {0} threads" 
 echo "Running Program"
--- TST/job_TST.sh
+++ TST/job_TST.sh
@@ -4,6 +4,7 @@
 #$ -pe smp 85
 #$-M j.teuffel@icloud.com -m ase
 #$ -cwd
+#$ -l h=!obelix11,!obelix12
 
 path="run_Time_Eval_Release_G85_Res500_Th2pi"
 export BOOST_ROOT="~/boost_1_72_0/"
@@ -11,7 +12,7 @@ echo $path
 module load intel/2020
 mkdir $path
 cd $path
-cmake cmake .. -DCMAKE_BUILD_TYPE=Release  -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=85 -DMKL_PARALEL=TRUE -DTARGET_ARCHITECTURE=skylake -DGRID=35 -DT_RES=500 -DT_ROT=1.0 -DT_C=-2.0 -DW_START=-3 -DW_END=3 -DW_RES=4 -DUSE_MAGMA=FALSE -DVERBOSE=TRUE  -DUSE_GPU=FALSE -DTIME_EVOLUTION=TRUE -DCMAKE_PREFIX_PATH="~/boost_1_72_0/"
+cmake cmake .. -DCMAKE_BUILD_TYPE=Release  -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  -DTHREADS=85 -DMKL_PARALEL=TRUE -DTARGET_ARCHITECTURE=westmere -DGRID=35 -DT_RES=500 -DT_ROT=1.0 -DT_C=-2.0 -DW_START=-3 -DW_END=3 -DW_RES=4 -DUSE_MAGMA=FALSE -DVERBOSE=TRUE  -DUSE_GPU=FALSE -DTIME_EVOLUTION=TRUE -DCMAKE_PREFIX_PATH="~/boost_1_72_0/"
 make -j 8
 echo $path
 echo "Running Program"
