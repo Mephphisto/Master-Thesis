69d172a09ef44299be6af0e063425d55b137ca5c
jakobteuffel <j.teuffel@web.de>
2021-02-11 14:14:00 +0100

    - Majoranize V3

--- TST/Majoranize.hpp
+++ TST/Majoranize.hpp
@@ -3,8 +3,8 @@
 
 double Error(cd gxx_gyy, double re_gxy, double Phi_A, double Phi_B, double Theta) {
     double ST = std::sin(Theta), CT = std::cos(Theta), S2_T = ST * ST, C2_T = CT * CT;
-    return 4.0 - 2.0 * (std::real((std::exp(cd(0, 2 * Phi_A)) * S2_T + std::exp(cd(0, 2 * Phi_B)) * C2_T) * gxx_gyy)
-                        + 2.0 * std::real(std::exp(cd(0, Phi_A + Phi_B)) * ST * CT * re_gxy));
+    return -(std::real((std::exp(cd(0, 2 * Phi_A)) * S2_T + std::exp(cd(0, 2 * Phi_B)) * C2_T) * gxx_gyy)
+             + 2.0 * std::real(std::exp(cd(0, Phi_A + Phi_B)) * ST * CT * re_gxy));
 }
 
 inline double dEdTheta(cd gxx_gyy, double re_gxy, double Phi_A, double Phi_B, double Theta) {
@@ -44,26 +44,25 @@ std::tuple<Vec_cd, Vec_cd> Majoranize(const Vec_cd &X, const Vec_cd &Y) {
 #ifdef DEBUG_ACTIVE
     std::cout << "gX*X = " << gX.dot(X.conjugate()) << " gY*Y = " << gY.dot(Y.conjugate()) << std::endl;
 #endif
-    cd gxx_gyy = 0.0;
+    cd gxx_gyy = gX.conjugate().dot(X) + gY.conjugate().dot(Y);
     const size_t N = X.size();
-    double re_gxy = 0.0;
-    for (size_t k = 0; k < X.size(); k++) {
-        gxx_gyy += X[k] * X[(k + N) % (2 * N)] + Y[k] * Y[(k + N) % (2 * N)];
-        re_gxy += std::real(X[(k + N) % (2 * N)] * Y[k]);
-    }
+    double re_gxy = gX.conjugate().dot(Y).real();
 
     double dEdPA, dEdPB, dEdPT;
 #ifdef DEBUG_ACTIVE
     std::cout << "Beginnig Majoranize, Error_0 = " << Error(gxx_gyy, re_gxy, 0.0, 0.0, 0.0) << std::endl
-              << " Check 4-2Re(X*gX)-2Re(Y*gY) = " << X.dot(X) + Y.dot(Y) + gX.dot(gX) + gY.dot(gY) - 2.0 * (std::real(
-            X.adjoint().dot(gX)) + std::real(Y.adjoint().dot(gY))) << std::endl
-              << " Check |gX-X^*|^2 + |gY-Y^*|^2 "
+              << " Check 4-2Re(X*gX)-2Re(Y*gY) = " << (X.dot(X) + Y.dot(Y) + gX.dot(gX) + gY.dot(gY) - 2.0 * (std::real(
+            X.adjoint().dot(gX)) + std::real(Y.adjoint().dot(gY)))).real() << std::endl
+              << " Check |gX-X^*|^2 + |gY-Y^*|^2  = "
               << (gX.conjugate() - X).squaredNorm() + (gY.conjugate() - Y).squaredNorm() << std::endl
               << " gxx_gyy = " << gxx_gyy << " re_gxy = " << re_gxy
               << std::endl;
 #endif
     for (size_t k = 0; k < 100; k++) {
         double Error_Old = Error(gxx_gyy, re_gxy, Phi_A, Phi_B, Theta);
+#ifdef DEBUG_ACTIVE
+        std::cout << "Beginnig Majoranize, Error = " << Error(gxx_gyy, re_gxy, Phi_A, Phi_B, Theta) << std::endl;
+#endif
         dEdPA = dEdPhi_A(gxx_gyy, re_gxy, Phi_A, Phi_B, Theta);
         dEdPB = dEdPhi_B(gxx_gyy, re_gxy, Phi_A, Phi_B, Theta);
         dEdPT = dEdTheta(gxx_gyy, re_gxy, Phi_A, Phi_B, Theta);
@@ -84,13 +83,15 @@ std::tuple<Vec_cd, Vec_cd> Majoranize(const Vec_cd &X, const Vec_cd &Y) {
     cd a = std::exp(cd(0, Phi_A)) * std::sin(Theta), b = std::exp(cd(0, Phi_B)) * std::cos(Theta);
     Vec_cd A = (a * X + b * Y), B = (b * X + a * Y);
 #ifdef DEBUG_ACTIVE
-    std::cout << "Ending Majoranize, Error_Final = " << Error(gxx_gyy, re_gxy, Phi_A, Phi_B, Theta)
-              << " Check |gX-X^*|^2 + |gY-Y^*|^2 "
-              << (GammaSwap(A) - A.conjugate()).squaredNorm() + (GammaSwap(B) - B.conjugate()).squaredNorm()
+    Vec_cd gA = GammaSwap(A), gB = GammaSwap(B);
+    std::cout << "Ending Majoranize, Error_Final = " << Error(gxx_gyy, re_gxy, Phi_A, Phi_B, Theta) << std::endl
+              << " Check 4-2Re(A*gA)-2Re(B*gB) = " << A.dot(A) + B.dot(B) + gA.dot(gA) + gB.dot(gB) - 2.0 * (std::real(
+            A.adjoint().dot(gA)) + std::real(B.adjoint().dot(gB))) << std::endl
+              << " Check |gA-A^*|^2 + |gB-B^*|^2  = "
+              << (gA.conjugate() - A).squaredNorm() + (gB.conjugate() - B).squaredNorm() << std::endl
               << std::endl;
 #endif
-    return
-            std::make_tuple(A, B);
+    return std::make_tuple(A, B);
 }
 
 std::tuple<Vec_cd, Vec_cd> Fermiize(const Vec_cd &X, const Vec_cd &Y) {
